5a865336445ac9bd620f80be2f020088bd59efaa
Joel Mandell <joelmandell@gmail.com>
2014-01-02 23:38:49 +0100

    view for showing forum subjects added

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from forum.models import Subject
+from forum.models import Thread
 
-# Register your models here.
+admin.site.register(Subject)
+admin.site.register(Thread)
--- forum/views.py
+++ forum/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from forumthread.models import Subject
-from forumthread.models import Thread
+from forum.models import Subject
+from forum.models import Thread
 
 def index(request):
 	subjects = Subject.objects.order_by('dateModified')
@@ -14,9 +14,9 @@ def index(request):
 	    })
 	return HttpResponse(template.render(context))
 
-def subject(request, subjectid):
+def subject(request, subject):
 	output=u"<h2>Hej på dig här kommer trådar som tillhör id {0}</h2>".format(subjectid)
-	all_threads=Subject.objects.get(id=subjectid).thread_set.all()
+	all_threads=Subject.objects.get(subjectTitle=subjectTitle).thread_set.all()
 
 	for t in all_threads:
 		output+=u"{0} <br />".format(t.threadTitle) 
