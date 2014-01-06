d887263543f37a38473061b3ab28cb1e649e3896
Joel Mandell <joelmandell@gmail.com>
2014-01-06 02:51:48 +0100

    Started learning to create tests

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from forum.models import Subject
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+class ForumTest(TestCase):
+  def setUp(self):
+    Subject.objects.create(dateModified=timezone.now(),subjectTitle="This is a test subject",subjectDescription="This is a description")
+    
+  def testCreationOfSubject(self):
+    subject = Subject.objects.get(id=1)
+    #Should return 1 = created subject success!
+    self.assertEqual(subject.id,1)
